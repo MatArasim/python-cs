4338595b2d67626d3e26a65b95887274d92fc744
MatArasim <arasimmateusz@gmail.com>
2023-04-26 09:53:59 +0200

    chart and code ajustments

--- LehmerGen.py
+++ LehmerGen.py
@@ -1,33 +1,33 @@
 import math
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 def giveNumbers(x, y, mod):
-  return (y*x) % mod
+  x= (x*y) % mod
+  return x
 
 
 x = 1
-m= 4056
+m= 4096
 a = 3
 k = 100
 k1 = k
-xn = np.array([])
+xn = []
+
 while (k1 > 0):
   x = giveNumbers(x, a, m)
   print(x/m)
   xn.append(x/m)
   k1 -= 1
-print(len(xn))
-# for x in points:
-#   plt.plot(x, linewidth=0)
 
+
+y =0
 fig, ax = plt.subplots()
 for x in xn:
     scale = 200.0
-    ax.scatter(x, s=scale)
+    ax.scatter(y, x)
+    y = y + 1
                
-
-ax.legend()
-ax.grid(True)
-
+ax.set_xlabel('Quantity', fontsize=15)
+ax.set_ylabel('Generated number', fontsize=15)
 plt.show()
